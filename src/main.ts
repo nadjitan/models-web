1b044356ecc86333f3a6afa71d47b6e2d1a78bc9
nadjitan <nadjitan@yahoo.com.ph>
2022-12-09 21:12:04 +0800

    edit str

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const loader = document.querySelector("#loader-container") as HTMLElement
 
 models.forEach(str => {
   const btn = document.createElement("button")
-  btn.textContent = str
+  btn.textContent = str.substring(str.lastIndexOf("/") + 1)
 
   btn.onclick = () => {
     render.setupObject(str)
